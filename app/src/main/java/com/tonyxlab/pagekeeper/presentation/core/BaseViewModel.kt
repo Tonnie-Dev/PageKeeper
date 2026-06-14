d940d24098e4a4efd7c656bbe94805078fc3c3a2
Tonnie <vontonnie@gmail.com>
2026-06-14 17:52:00 +0300

    Accept initialState in constructor inside BaseViewModel

--- app/src/main/java/com/tonyxlab/pagekeeper/presentation/core/BaseViewModel.kt
+++ app/src/main/java/com/tonyxlab/pagekeeper/presentation/core/BaseViewModel.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
@@ -18,11 +17,12 @@ import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.cancellation.CancellationException
 
-abstract class BaseViewModel<S : UiState, E : UiEvent, A : ActionEvent> : ViewModel() {
-    protected abstract val initialState: S
+abstract class BaseViewModel<S : UiState, E : UiEvent, A : ActionEvent>(
+    initialState: S
+) : ViewModel() {
 
-    private val _uiState: MutableStateFlow<S> by lazy { MutableStateFlow(initialState) }
-    val uiState: StateFlow<S> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(initialState)
+    val uiState = _uiState.asStateFlow()
 
     private val _actionEvent = Channel<A>()
     val actionEvent = _actionEvent.receiveAsFlow()
