c596e96a47faa920db06e5733747c4cf3388d2a5
Tonnie <vontonnie@gmail.com>
2026-07-19 13:42:11 +0300

    Use Decoupled Bubble

--- app/src/main/java/com/tonyxlab/pagekeeper/presentation/screens/read/components/FontSizeSlider.kt
+++ app/src/main/java/com/tonyxlab/pagekeeper/presentation/screens/read/components/FontSizeSlider.kt
@@ -2,6 +2,7 @@
 
 package com.tonyxlab.pagekeeper.presentation.screens.read.components
 
+import android.R.attr.maxWidth
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -35,15 +36,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tonyxlab.pagekeeper.R
@@ -52,6 +56,7 @@ import com.tonyxlab.pagekeeper.presentation.screens.read.handling.ReaderFontSize
 import com.tonyxlab.pagekeeper.presentation.theme.BgMain
 import com.tonyxlab.pagekeeper.presentation.theme.PageKeeperTheme
 import com.tonyxlab.pagekeeper.presentation.theme.Primary
+import com.tonyxlab.pagekeeper.presentation.theme.TitleLargeBold
 import com.tonyxlab.pagekeeper.presentation.theme.spacing
 import kotlin.math.roundToInt
 
@@ -71,17 +76,25 @@ fun FontSizeControlPanel(
         sliderValue = fontSize.coerceIn(ReaderFontSize.range)
     }
 
-    Surface(
-            modifier = modifier.fillMaxWidth(),
-            color = MaterialTheme.colorScheme.surfaceContainer,
+    Box(
+            modifier = modifier
+                    .fillMaxWidth()
+                    .background(
+
+                            color = MaterialTheme.colorScheme.surfaceContainer,
+
+                            )
     ) {
         Row(
                 modifier = Modifier
                         .fillMaxWidth()
                         .padding(
                                 horizontal = 24.dp,
-                                vertical = 20.dp,
-                        ),
+                                //vertical = 20.dp,
+                        )
+                        .padding(top = 28.dp, bottom = 12.dp)
+                ,
+
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(16.dp),
         ) {
@@ -100,7 +113,6 @@ fun FontSizeControlPanel(
                     },
                     onClick = {
 
-
                         val newValue = (sliderValue - 1f)
                                 .coerceAtLeast(ReaderFontSize.MIN)
 
@@ -111,7 +123,7 @@ fun FontSizeControlPanel(
             )
 
 
-            FontSizeSlider(
+            FontSizeSliders(
                     value = sliderValue,
                     onValueChange = { newValue ->
                         sliderValue = newValue
@@ -137,7 +149,6 @@ fun FontSizeControlPanel(
                     },
                     onClick = {
 
-
                         val newValue = (sliderValue + 1f)
                                 .coerceAtMost(ReaderFontSize.MAX)
 
@@ -147,12 +158,10 @@ fun FontSizeControlPanel(
                     }
             )
 
-
         }
     }
 
 }
-
 
 @Composable
 private fun FontSizeSlider(
@@ -173,8 +182,11 @@ private fun FontSizeSlider(
             inactiveTickColor = MaterialTheme.colorScheme.primary,
     )
 
+    // Use Box with Constraints to calculate the exact width of the track
+    // to position of the floating bubble horizontally
+
     Box(
-            modifier = modifier.height(112.dp),
+           // modifier = modifier.height(112.dp),
             contentAlignment = Alignment.BottomCenter,
     ) {
         Slider(
@@ -225,7 +237,9 @@ private fun FontSizeSliderThumb(
 ) {
     Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.offset(y = (-26).dp),
+            modifier = Modifier
+                    .offset(y = (-26).dp)
+                    .background(Color.Red),
     ) {
         Surface(
                 modifier = Modifier.size(76.dp),
@@ -238,7 +252,8 @@ private fun FontSizeSliderThumb(
                     contentAlignment = Alignment.Center,
             ) {
                 Text(
-                        text = value.roundToInt().toString(),
+                        text = value.roundToInt()
+                                .toString(),
                         style = MaterialTheme.typography.displaySmall,
                         fontWeight = FontWeight.Bold,
                         color = MaterialTheme.colorScheme.onSurface,
@@ -252,7 +267,7 @@ private fun FontSizeSliderThumb(
                 interactionSource = interactionSource,
                 colors = colors,
                 modifier = Modifier
-                        .width(12.dp)
+                        .width(6.dp)
                         .height(52.dp),
         )
     }
@@ -278,7 +293,6 @@ private fun FontSizeAdjustmentButton(
     }
 
 }
-
 
 /*
 @Composable
@@ -343,58 +357,109 @@ fun ReaderSliderBar(
 
 */
 @Composable
-private fun FontSizeSlider(
-    fontSizeSp: Float,
-    onFontSizeChange: (Float) -> Unit,
+private fun FontSizeSliders(
+    value: Float,
+    onValueChange: (Float) -> Unit,
+    onValueChangeFinished: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val sliderColors = SliderDefaults.colors(
+            thumbColor = MaterialTheme.colorScheme.primary,
+            activeTrackColor = MaterialTheme.colorScheme.primary,
+            inactiveTrackColor = MaterialTheme.colorScheme.surfaceContainerHighest,
+            activeTickColor = MaterialTheme.colorScheme.primary,
+            inactiveTickColor = MaterialTheme.colorScheme.primary,
+    )
+
+    // Use Box with Constraints to calculate the exact width of the track
+    // to position of the floating bubble horizontally
+
     BoxWithConstraints(
             modifier = modifier.height(SliderHeight),
             contentAlignment = Alignment.BottomCenter
     ) {
-        val fraction = (fontSizeSp - MinFontSizeSp) / (MaxFontSizeSp - MinFontSizeSp)
-        val bubbleOffset = (maxWidth - BubbleSize) * fraction.coerceIn(0f, 1f)
+
+        val minVal = ReaderFontSize.MIN
+        val maxVal = ReaderFontSize.MAX
+
+        val range = maxVal - minVal
+        val thumbWidth = 0.dp
+        // Calculate how far the slider is
+
+        val sliderFraction = if (range > 0f) (value - minVal)/range else 0f
+
+        // Account for the bubble size to center nicely above the thumb
+
+        val thumbCenterX =
+            (thumbWidth / 2) +
+                    ((maxWidth - thumbWidth) * sliderFraction)
+
+        val bubbleOffsetX =
+            thumbCenterX - (BubbleSize.width / 2)
+
+
 
         Surface(
                 modifier = Modifier
-                        .offset(x = bubbleOffset)
-                        .size(BubbleSize)
-                        .align(Alignment.TopStart),
+                        .align(Alignment.TopStart)
+                        .offset(x = bubbleOffsetX,
+                                y = (-54).dp
+                        )
+                        .size(BubbleSize),
+
                 shape = CircleShape,
                 color = MaterialTheme.colorScheme.surface,
-                shadowElevation = 3.dp
+                shadowElevation = 8.dp,
+                tonalElevation = 2.dp
         ) {
             Box(contentAlignment = Alignment.Center) {
                 Text(
-                        text = fontSizeSp.roundToInt()
+                        text = value.roundToInt()
                                 .toString(),
-                        style = MaterialTheme.typography.titleMedium,
+                        style = MaterialTheme.typography.TitleLargeBold,
                         color = MaterialTheme.colorScheme.onSurface,
-                        fontSize = 20.sp
+
                 )
             }
         }
 
         Slider(
-                value = fontSizeSp,
-                onValueChange = onFontSizeChange,
-                valueRange = MinFontSizeSp..MaxFontSizeSp,
-                steps = FontSizeSliderSteps,
-                modifier = Modifier
-                        .fillMaxWidth()
-                        .align(Alignment.BottomCenter),
-                colors = SliderDefaults.colors(
-                        thumbColor = Primary,
-                        activeTrackColor = Primary,
-                        inactiveTrackColor = MaterialTheme.colorScheme.surfaceVariant
-                )
+                modifier = Modifier.fillMaxWidth(),
+                value = value,
+                onValueChange = onValueChange,
+                onValueChangeFinished = onValueChangeFinished,
+                valueRange = ReaderFontSize.range,
+                steps = 0,
+                interactionSource = interactionSource,
+                colors = sliderColors,
+                thumb = {
+                    Box(modifier = Modifier
+                            .width(SliderWidth)
+                            .height(SliderHeight)
+                            .background(color =MaterialTheme.colorScheme.primary,
+                                    shape = MaterialTheme.shapes.extraSmall)
+
+                    )
+                },
+                track = { sliderState ->
+
+                    SliderDefaults.Track(
+                            sliderState = sliderState,
+                            colors = sliderColors,
+                            thumbTrackGapSize = 0.dp,
+                            trackInsideCornerSize = 8.dp
+                    )
+                }
         )
     }
 }
 
 private val FontSizeButtonSize = 38.dp
-private val SliderHeight = 58.dp
-private val BubbleSize = 50.dp
+private val SliderWidth = 6.dp
+private val SliderHeight = 44.dp
+private val BubbleSize = DpSize(64.dp, 54.dp)
 private const val MinFontSizeSp = 16f
 private const val MaxFontSizeSp = 24f
 private const val FontSizeSliderSteps = 1
@@ -420,8 +485,7 @@ private fun ReaderBottomBarPreview() {
                     onFontSizeChange = {},
                     onFontSizeChangeFinished = {},
 
-
-            )
+                    )
         }
     }
 }
